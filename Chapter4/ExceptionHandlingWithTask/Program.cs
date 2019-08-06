78c9989b6c1172c673e7e68306b30f692c66042e
Rishabh Verma <rishabhv@live.com>
2019-08-06 04:45:04 +0000

    More sample for Chapter 4

---
+++ Chapter4/ExceptionHandlingWithTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingWithTask
+{
+    /// <summary>
+    /// Demonstrates exception handling with Tasks.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = new Task<int>(MethodThatThrowsException, null);
+
+            try
+            {                
+                task.Start();
+                Console.WriteLine($"The result is {task.Result}"); //// task.Wait() would have the same effect.
+            }
+            catch (AggregateException ex)
+            {
+                ex.Handle((e) =>
+                {
+                    if (e is InvalidOperationException)
+                    {
+                        Console.WriteLine($"Caught Exception - {e.Message}");
+                        return true;
+                    }
+
+                    return false;
+                });
+            }
+
+            Console.ReadLine();
+        }
+
+        static int MethodThatThrowsException(object state)
+        {
+            throw new InvalidOperationException("Throwing Exception for demonstrating Exception handling in Tasks");
+        }
+    }
+}
---
+++ Chapter4/TaskWithResult/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskWithResult
+{
+    /// <summary>
+    /// Demonstrates the task with result.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1000;
+            Task<int> task = new Task<int>(SumOfNumbers, number);
+            task.Start();
+            task.Wait();
+            Console.WriteLine($"The sum of first {number} is {task.Result}");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Calculates the sum of numbers from 0 to the specified number.
+        /// </summary>
+        /// <param name="numberTo">The number till which sum needs to be calculated.</param>
+        /// <returns></returns>
+        static int SumOfNumbers(object numberTo)
+        {
+            var num = (int)numberTo;
+            int sum = 0;
+            if (num > 0)
+            {
+                for (int i = 0; i <= num; i++)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
