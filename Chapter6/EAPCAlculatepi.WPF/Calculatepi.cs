3ac283bc499d91972018cd4d3aa7c252e000f1e2
Ravindra Akella <ravindra.a@outlook.com>
2019-08-25 13:25:15 +0530

    TAP wrapper over EAP WPF app

---
+++ Chapter6/EAPCAlculatepi.WPF/Calculatepi.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EAPCAlculatepi.WPF
+{
+    public delegate void CalculatepiCompletedEventHandler(object sender, CalculatepiCompletedEventArgs e);
+    public class Calculatepi
+    {
+        //Completion event handler
+        public event CalculatepiCompletedEventHandler CalculatepiCompleted;
+
+        //delegate to call the synchronous operation in async way (in this case we are using task.run)
+        private delegate void CalculationEventHandler(int numSteps, object sender, AsyncOperation asyncOp);
+
+        //Delegate to handle callback and raise completion event
+        private SendOrPostCallback onCompletedDelegate;
+
+        public Calculatepi()
+        {
+            onCompletedDelegate = new SendOrPostCallback(CalculationCompleted);
+        }
+
+        /// <summary>
+        /// callback method
+        /// </summary>
+        private void CalculationCompleted(object operationState)
+        {
+            CalculatepiCompletedEventArgs e = operationState as CalculatepiCompletedEventArgs;
+
+            if (CalculatepiCompleted != null)
+            {
+                CalculatepiCompleted(this, e);
+            }
+        }
+
+        //Dicstionary to handle multiple tasks innovcation, uniquely identify each opearation as one dictionary element
+        private HybridDictionary parallelTasks = new HybridDictionary();
+
+        /// <summary>
+        /// Calculate pi async
+        /// </summary>
+        public void CalculatepiAsync(int numsteps, object sender, object operationState)
+        {
+            AsyncOperation asyncOp = AsyncOperationManager.CreateOperation(operationState);
+
+            //Locking for thread safety
+            lock (parallelTasks.SyncRoot)
+            {
+                if (parallelTasks.Contains(operationState))
+                {
+                    throw new ArgumentException("User state parameter must be unique", "userState");
+                }
+
+                parallelTasks[operationState] = asyncOp;
+            }
+
+            CalculationEventHandler worker = new CalculationEventHandler(Calculatepivalue);
+
+            //Execute process Asynchronously
+            Task.Run(() => worker(numsteps, sender, asyncOp));
+        }
+
+        /// <summary>
+        /// Synchronous method to calculate pi
+        /// </summary>
+        void Calculatepivalue(int numsteps, object sender, AsyncOperation asyncOp)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            numsteps++;
+
+            uint[] value = new uint[numsteps * 10 / 3 + 2];
+            uint[] rem = new uint[numsteps * 10 / 3 + 2];
+
+            uint[] pi = new uint[numsteps];
+
+            for (int j = 0; j < value.Length; j++)
+                value[j] = 20;
+
+            for (int i = 0; i < numsteps; i++)
+            {
+                Thread.Sleep(5);
+                if (TaskCanceled(asyncOp.UserSuppliedState))
+                {
+                    break;
+                }
+                uint carryForward = 0;
+                for (int j = 0; j < value.Length; j++)
+                {
+                    uint number = (uint)(value.Length - j - 1);
+                    uint pow = number * 2 + 1;
+
+                    value[j] += carryForward;
+
+                    uint quotient = value[j] / pow;
+                    rem[j] = value[j] % pow;
+
+                    carryForward = quotient * number;
+                }
+
+
+                pi[i] = (value[value.Length - 1] / 10);
+
+
+                rem[value.Length - 1] = value[value.Length - 1] % 10; ;
+
+                for (int j = 0; j < value.Length; j++)
+                    value[j] = rem[j] * 10;
+            }
+
+            var result = "";
+
+            uint c = 0;
+
+            for (int i = pi.Length - 1; i >= 0; i--)
+            {
+                pi[i] += c;
+                c = pi[i] / 10;
+
+                result = (pi[i] % 10).ToString() + result;
+            }
+            result = result.Substring(0, 1) + "." + result.Substring(1, result.Length - 1);
+
+
+            //rasie callback
+            CalculatepiCompletedEventArgs e = new CalculatepiCompletedEventArgs(result, timer.ElapsedMilliseconds, sender, null, TaskCanceled(asyncOp.UserSuppliedState), asyncOp.UserSuppliedState);
+            asyncOp.PostOperationCompleted(onCompletedDelegate, e);
+
+            lock (parallelTasks.SyncRoot)
+            {
+                parallelTasks.Remove(asyncOp.UserSuppliedState);
+            }
+
+            timer.Stop();
+        }
+
+        // This method cancels a pending asynchronous operation.
+        public void CancelAsync(object operationState)
+        {
+            AsyncOperation asyncOp = parallelTasks[operationState] as AsyncOperation;
+            if (asyncOp != null)
+            {
+                lock (parallelTasks.SyncRoot)
+                {
+                    parallelTasks.Remove(operationState);
+                }
+            }
+        }
+
+        //Utility method to check the task status
+        private bool TaskCanceled(object operationState)
+        {
+            return (parallelTasks[operationState] == null);
+        }
+    }
+
+    public class CalculatepiCompletedEventArgs : AsyncCompletedEventArgs
+    {
+        public string Result { get; private set; }
+        public long TimeTaken { get; private set; }
+
+        public Object Sender { get; private set; }
+
+        public CalculatepiCompletedEventArgs(string value, long TimeTaken, object sender, Exception e, bool canceled, object state) : base(e, canceled, state)
+        {
+            this.Result = value;
+            this.TimeTaken = TimeTaken;
+            this.Sender = sender;
+        }
+    }
+
+}
---
+++ Chapter6/EAPCAlculatepi.WPF/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EAPCAlculatepi.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Calculatepi pi;
+        CancellationTokenSource cts = null;
+        CalculatepiCompletedEventArgs calculatepiCompletedEventArgs = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //pi = new Calculatepi();
+            //pi.CalculatepiCompleted += new CalculatepiCompletedEventHandler(Pi_CalculatePrimeCompleted);
+        }
+
+        private void Calculatepi1000_Click(object sender, RoutedEventArgs e)
+        {
+            //if (calculatepi1000.Content.ToString().Contains("pi"))
+            //{
+            //    calculatepi1000.Content = "Cancel";
+            //    pi.CalculatepiAsync(1000, sender, "Calculate pi (1000)");
+            //}
+            //else
+            //{
+            //    pi.CancelAsync("Calculate pi (1000)");
+            //    calculatepi1000.Content = "Calculate pi (1000)";
+            //}
+        }
+
+        void Pi_CalculatePrimeCompleted(object sender, CalculatepiCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+            {
+                output.Text += $"{e.UserState.ToString()} places - {e.Result}, time taken is {e.TimeTaken.ToString()} Milliseconds{Environment.NewLine}";
+            }
+            else
+            {
+                output.Text += $"{e.UserState.ToString()} places is cancelled, time taken is {e.TimeTaken.ToString()} Milliseconds{Environment.NewLine}";
+            }
+            ((Button)e.Sender).Content = e.UserState.ToString();
+        }
+
+        private void Calculatepi900_Click(object sender, RoutedEventArgs e)
+        {
+            //if (calculatepi900.Content.ToString().Contains("pi"))
+            //{
+            //    calculatepi900.Content = "Cancel";
+            //    pi.CalculatepiAsync(900, sender, "Calculate pi (900)");
+            //}
+            //else
+            //{
+            //    pi.CancelAsync("Calculate pi (900)");
+            //    calculatepi900.Content = "Calculate pi (900)";
+            //}
+        }
+
+        private async void Calculatepi1000TAP_Click(object sender, RoutedEventArgs e)
+        {
+            Calculatepi calculatepi = new Calculatepi();
+            calculatepi1000TAP.Content = "Cancel";
+            //On clicking of Cancel checking to cancel opearation 
+            if (cts != null)
+            {
+                cts.Cancel();
+                cts = null;
+                return;
+            }
+
+            cts = new CancellationTokenSource();            
+
+            //Cancellation needs to be handled gracefully
+            try
+            {
+                calculatepiCompletedEventArgs = await TAPWrappertoAPMAsync(calculatepi, 1000, sender, "Calculate pi (1000) TAP", cts.Token);
+                output.Text += $"{calculatepiCompletedEventArgs.UserState.ToString()} - {calculatepiCompletedEventArgs.Result}, time taken is {calculatepiCompletedEventArgs.TimeTaken.ToString()} Milliseconds{Environment.NewLine}";
+            }
+            catch (OperationCanceledException)
+            {
+                output.Text += $"Calculate pi(1000) TAP is cancelled";
+            }
+            finally
+            {
+                cts = null;
+                calculatepiCompletedEventArgs = null;
+            }
+
+            calculatepi1000TAP.Content = "Calculate pi(1000) TAP";
+        }
+
+        private Task<CalculatepiCompletedEventArgs> TAPWrappertoAPMAsync(Calculatepi calculatepi, int numsteps, object sender, object operationState, CancellationToken token)
+        {
+            var tcs = new TaskCompletionSource<CalculatepiCompletedEventArgs>();
+            //Delegate on cancellation token when there is a cancellation, executes on calling thread's context in this case UI
+            token.Register(() =>
+            {
+                calculatepi.CancelAsync(operationState);
+            });
+            calculatepi.CalculatepiCompleted += (_, e) =>
+            {
+                if (e.Cancelled)
+                {
+                    tcs.TrySetCanceled();
+                    return;
+                }
+                else if (e.Error != null)
+                {
+                    tcs.TrySetException(e.Error);
+                    return;
+                }
+                else
+                    tcs.TrySetResult(e);
+            };
+
+            // Register for the event and *then* start the operation.
+            calculatepi.CalculatepiAsync(numsteps, sender, operationState);
+            return tcs.Task;
+        }
+    }
+}
