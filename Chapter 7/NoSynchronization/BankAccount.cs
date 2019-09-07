597077dae465dacf63bccceec3c3f34ed5532254
Ravindra Akella <ravindra.a@outlook.com>
2019-09-08 00:44:59 +0530

    why Synchronization  theory and example

---
+++ Chapter 7/NoSynchronization/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoSynchronization
+{
+    public class BankAccount
+    {
+        private long accountBalance;
+        private int numberOfTransactions;
+
+        public int NumberOfTransactions
+        {
+            get
+            {
+                return numberOfTransactions;
+            }
+        }
+
+        //Lock
+        object locker = new object();
+
+        public BankAccount(long initialAccountBalance)
+        {
+            this.accountBalance = initialAccountBalance;
+            numberOfTransactions = 0;
+        }
+
+        /// <summary>
+        /// Add money to account through multiple transactions
+        /// </summary>        
+        public async Task AddMoneyToAccount()
+        {
+            var tasks = new Task[50];
+
+            for (int i = 1; i <= tasks.Length; i++)
+            {
+                tasks[i - 1] = AddBalanceToAcccount(i);
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        async Task AddBalanceToAcccount(long amount)
+        {
+            await Task.Delay(1);
+            lock (locker)
+            {
+                accountBalance = accountBalance + amount;
+                numberOfTransactions = numberOfTransactions + 1;
+            }
+        }
+
+        public long ShowBalance()
+        {
+            return this.accountBalance;
+        }
+    }
+}
---
+++ Chapter 7/NoSynchronization/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NoSynchronization
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount(0);
+            Console.WriteLine($"Initial Balance {bankAccount.ShowBalance()}");
+            await bankAccount.AddMoneyToAccount();
+            Console.WriteLine($"Current Balance {bankAccount.ShowBalance()}, total number of transactions - {bankAccount.NumberOfTransactions}");
+            Console.Read();
+        }
+    }
+}
