aba41f553f78e5b93faa40c7bf3dc69fb15834c8
Ravindra Akella <ravindra.a@outlook.com>
2019-09-23 19:58:07 +0530

    Mutex theory

--- Chapter 7/NoSynchronization/BankAccount.cs
+++ Chapter 7/NoSynchronization/BankAccount.cs
@@ -24,6 +24,7 @@ namespace NoSynchronization
 
         //Lock
         object locker = new object();
+        bool isAcquired = false;
 
         public BankAccount(long initialAccountBalance)
         {
@@ -37,28 +38,62 @@ namespace NoSynchronization
         public async Task AddMoneyToAccountAsync()
         {
             var tasks = new Task[50];
+             for (int i = 1; i <= tasks.Length; i++)
+                {
+                    tasks[i - 1] = AddBalanceToAcccount(i);
+                }
 
-            for (int i = 1; i <= tasks.Length; i++)
-            {
-                tasks[i - 1] = AddBalanceToAcccount(i);
-            }
-            
-            await Task.WhenAll(tasks);
-        }
-
-        async Task AddBalanceToAcccount(long amount)
-        {
-            await Task.Delay(1);
-            lock (locker)
-            {
-                accountBalance = accountBalance + amount;
-                numberOfTransactions = numberOfTransactions + 1;
-            }
+            //Thread.Sleep(5000);
+            //for (int i = 1; i <= tasks.Length; i++)
+            //{                
+            //    Console.WriteLine(tasks[i - 1].Status);
+            //}
+           await Task.WhenAll(tasks);
         }
 
         public long ShowBalance()
         {
             return this.accountBalance;
         }
+
+        #region Private methods
+        async Task AddBalanceToAcccount(long amount)
+        {
+            await Task.Delay(1);
+            //long modifiedamount = 0;
+            //lock (locker)
+            //{
+            //    //Thread.Sleep(2000);
+            //    modifiedamount = amount / (amount - 20);
+            //    accountBalance = accountBalance + amount;
+            //    numberOfTransactions = numberOfTransactions + 1;
+            //}
+
+            bool lockAcquired = false;
+            try
+            {
+                Monitor.Enter(locker, ref lockAcquired);
+                //modifiedamount = amount / (amount - 20);
+                accountBalance = accountBalance + amount;
+                numberOfTransactions = numberOfTransactions + 1;
+            }
+            finally
+            {
+                if (lockAcquired)
+                {
+                    Monitor.Exit(locker);
+                }
+            }
+
+        }
+
+        void AvailableThreads()
+        {
+            int worker, io;
+            ThreadPool.GetAvailableThreads(out worker, out io);
+            Console.WriteLine("   Worker threads: {0:N0}", worker);
+        }
+        #endregion
+
     }
 }
