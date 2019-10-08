e066e453e7b2f5f216e08c060cc345160b5c07dc
Ravindra Akella <ravindra.a@outlook.com>
2019-10-09 01:02:00 +0530

    AutoResetEvent theory and sample

---
+++ Chapter 7/StockTradingUsingAutoResetEvent/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace StockTradingUsingAutoResetEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter buy price of stock XYZ");
+            StockTrading stockTrading = new StockTrading(false);
+
+            //Thread to place order
+            Thread placeOrder = new Thread(stockTrading.PlaceOrder);
+            placeOrder.Start();
+
+            //Thread that checks for current price and completes order
+            Thread validatePrice = new Thread(() =>
+            {
+                Random randomCurrentPriceofStock = new Random();
+                while (!stockTrading.StockPurchased)
+                {
+                    stockTrading.currentStockPriceOfXYZ = randomCurrentPriceofStock.Next(1, 5);
+                    stockTrading.ValidatePrice();
+                    Thread.Sleep(1000); // Wait for input before execuiting next iteration or else screen will overflow
+                }
+            });
+            validatePrice.Start();
+
+        }
+    }
+}
