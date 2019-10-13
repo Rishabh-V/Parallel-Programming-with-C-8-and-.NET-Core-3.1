deee9121df07c650d882eb6edb449c0de81777a8
Rishabh Verma <rishabhv@live.com>
2019-10-13 13:28:10 +0000

    Async await

--- Chapter4/AsyncAwait/Program.cs
+++ Chapter4/AsyncAwait/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace AsyncAwait
 {
@@ -9,7 +12,58 @@ namespace AsyncAwait
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            // Set the url to a website from which content needs to be downloaded.
+            string url = "https://bpbonline.com/collections/c-sharp";
+            // Path where downloaded data needs to be saved.
+            string path = "C:\\Rishabh\\download.txt";
+
+            // Ensure that the directory of the file path exists.
+            var directory = System.IO.Path.GetDirectoryName(path);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            // Call the method.
+            DownloadData(url, path);
         }
+
+        private static void DownloadData(string url, string path)
+        {
+            // Create a new web client object
+            using (WebClient client = new WebClient())   //1
+            {
+                // Add user-agent header to avoid forbidden errors.
+                client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64)"); //2
+                // download data from Url
+                byte[] data = client.DownloadData(url);  //3
+                // Write data in file.
+                using (var fileStream = File.OpenWrite(path))  //4
+                {
+                    fileStream.Write(data, 0, data.Length); //5
+                }
+            }
+        }
+                     
+        private static async Task DownloadDataAsync(string url, string path)
+        {
+            // Create a new web client object
+            using (WebClient client = new WebClient()) //1
+            {
+                // Add user-agent header to avoid forbidden errors.
+                client.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64)"); //2
+                // download data from Url
+                byte[] data = await client.DownloadDataTaskAsync(url); //3
+                // Write data in file.
+                using (var fileStream = File.OpenWrite(path)) //4
+                {
+                    await fileStream.WriteAsync(data, 0, data.Length); //5
+                }
+            }
+        }
+
+
+
     }
 }
