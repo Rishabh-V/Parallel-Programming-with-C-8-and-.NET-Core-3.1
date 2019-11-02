8570f9989832e133fcc0342f225f28906fc1fad9
Ravindra Akella <ravindra.a@outlook.com>
2019-11-03 00:30:42 +0530

    unit test basics and asyc unit tests

---
+++ Chapter 8/MathClass/MathClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class MathClass
+    {
+        public int Divide(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+            {
+                //return numerator / denominator;
+                int remainder;
+                return Math.DivRem(numerator, denominator, out remainder);
+            }
+        }
+
+        public async Task<int> DivideAsync(int numerator, int denominator)
+        {
+            var t = Task.Run(() =>
+            {
+                return Divide(numerator, denominator);
+            });
+            return await t;
+        }
+
+
+    }
+}
---
+++ Chapter 8/MathClassUnitTest/MathClassUnitTest.cs
@@ -0,0 +1,26 @@
+﻿namespace CalculatorUnitTest
+{
+    using Calculator;
+    using System.Threading.Tasks;
+    using Xunit;
+    public class MathClassUnitTest
+    {
+        [Fact]
+        public void TestDivide()
+        {
+            var mathClass = new MathClass();
+            int output = 3; //mock data
+            var result = mathClass.Divide(6, 2);
+            Assert.Equal(output, result);
+        }
+
+        [Fact]
+        public async Task TestDivideAsync()
+        {
+            var mathClass = new MathClass();
+            var result = await mathClass.DivideAsync(6, 2);
+            Assert.Equal(3, result);
+        }
+
+    }
+}
