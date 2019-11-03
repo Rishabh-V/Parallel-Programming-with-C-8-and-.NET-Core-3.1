fc5a7ebb6f0be84f0f58b2b4fb8160461a67bb33
Ravindra Akella <ravindra.a@outlook.com>
2019-11-04 00:35:19 +0530

    exception handling unit tests

--- Chapter 8/MathClass/MathClass.cs
+++ Chapter 8/MathClass/MathClass.cs
@@ -1,31 +1,65 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Calculator
 {
     public class MathClass
     {
-        public int Divide(int numerator, int denominator)
+        public int Divide(int numerator, int? denominator)
         {
             if (denominator == 0)
             {
                 throw new DivideByZeroException();
             }
+            else if (!denominator.HasValue)
+            {
+                throw new ArgumentNullException();
+            }
             else
             {
                 //return numerator / denominator;
                 int remainder;
-                return Math.DivRem(numerator, denominator, out remainder);
+                return Math.DivRem(numerator, denominator.Value, out remainder);
             }
         }
 
         public async Task<int> DivideAsync(int numerator, int denominator)
         {
-            var t = Task.Run(() =>
+            try
             {
-                return Divide(numerator, denominator);
-            });
-            return await t;
+                var t = Task.Run(() =>
+                {
+                    return Divide(numerator, denominator);
+                });
+                return await t;
+                //return await t.ConfigureAwait(false);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private async Task DownloadLargeFilAsync()
+        {
+            byte[] buffer = new byte[8192];
+            using (HttpClient client = new HttpClient())
+            {
+
+                string url = "https://github.com/Ravindra-a/largefile/archive/master.zip";
+                using (HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+                    long totalLength = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : 34632982; //Once in a while github returns response without content length header 
+                    //hence in that case defaulting to actual file size
+                    using (Stream stream = await response.Content.ReadAsStreamAsync())
+                    {
+                            int dataToRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    }
+                }
+            }
         }
 
 
--- Chapter 8/MathClassUnitTest/MathClassUnitTest.cs
+++ Chapter 8/MathClassUnitTest/MathClassUnitTest.cs
@@ -1,6 +1,7 @@
 ﻿namespace CalculatorUnitTest
 {
     using Calculator;
+    using System;
     using System.Threading.Tasks;
     using Xunit;
     public class MathClassUnitTest
@@ -20,6 +21,30 @@
             var mathClass = new MathClass();
             var result = await mathClass.DivideAsync(6, 2);
             Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void TestDivideAsyncUsingResult()
+        {
+            var mathClass = new MathClass();
+            var result = mathClass.DivideAsync(6, 2).GetAwaiter().GetResult();
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public async Task TestDivideByZeroException()
+        {
+            var mathClass = new MathClass();
+            var result = mathClass.DivideAsync(6, 0);
+            await Assert.ThrowsAsync<DivideByZeroException>(async () => await result);
+        }
+
+        [Fact]
+        public async Task TestDivideByGenericException()
+        {
+            var mathClass = new MathClass();
+            var result = mathClass.DivideAsync(6, null);
+            await Assert.ThrowsAnyAsync<Exception>(async () => await result);
         }
 
     }
