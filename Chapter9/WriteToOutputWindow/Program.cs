33c10d8db8180ac1eb68a9dc5662ee1eea273220
Rishabh Verma <rishabhv@live.com>
2019-11-11 18:03:00 +0000

    Chapter9

---
+++ Chapter9/WriteToOutputWindow/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DebuggingPrimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output window
+            Debug.WriteLine($"Hello world via Debug statement!");
+            Trace.WriteLine($"Hello world via Trace statement!");
+            Console.WriteLine("Hello World!");
+
+            // Immediate window
+            int aNumber = 100 / 5;
+            float anotherNumber = 200 / 66;                    
+            // See intellisense and values in immediate window.
+                                                               
+            Console.ReadLine();
+        }
+    }
+}
