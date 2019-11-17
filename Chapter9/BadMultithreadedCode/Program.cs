0bb36b976b8988c5ef773631d7771905c095f359
Rishabh Verma <rishabhv@live.com>
2019-11-17 20:45:54 +0000

    Chapter 9

---
+++ Chapter9/BadMultithreadedCode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BadMultithreadedCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Look up data to be populated
+            Dictionary<int, int> lookupData = new Dictionary<int, int>();
+            Console.WriteLine("This sample demonstrates bad code written using multi-threading. We will use this sample learn to collect and analyze dumps via different tools !");
+            // Dumps are collected at a point of time. To make demonstration easier, we will perform operations inside an infinite loop.
+            while (true)
+            {
+                try
+                {
+                    Parallel.For(0, 1000000, new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount }, (i) =>
+                             {
+                                 //// Process data returned from API and add it to a collection.
+                                 Thread.Sleep(20);
+                                 lookupData.Add(i, GetValueFromAPI(i));
+                             });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception occurred in processing data {ex.ToString()}");
+                }
+            }
+        }
+
+        static int GetValueFromAPI(int i)
+        {
+            //// Simulate delay of 1sec for fetching data from DB/API
+            Thread.Sleep(1000);
+            var result = new Random().Next(0, i);
+            return result;             
+        }
+    }
+}
---
+++ Chapter9/ProfilingDemo/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProfilingDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static HttpClient httpClient = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            this.txtUrl.Text = "https://bpbonline.com/collections/c-sharp";
+        }
+
+        private async void BtnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            await GoodCodeAsync();
+        }
+
+        private void BadCode()
+        {
+            try
+            {
+                httpClient.BaseAddress = new Uri(this.txtUrl.Text);
+                httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64)");
+                var result = httpClient.GetAsync("/").Result;
+                result.EnsureSuccessStatusCode();
+                var output = result.Content.ReadAsStringAsync().Result;
+                this.lstData.Items.Add(output);
+                this.lstData.UpdateLayout();
+            }
+            catch (Exception ex)
+            {
+                this.lstData.Items.Add(ex.ToString());
+            }
+        }
+
+        private async Task GoodCodeAsync()
+        {
+            try
+            {
+                httpClient.BaseAddress = new Uri(this.txtUrl.Text);
+                httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64)");
+                var result = await httpClient.GetAsync("/");
+                result.EnsureSuccessStatusCode();
+                var output = await result.Content.ReadAsStringAsync();
+                this.lstData.Items.Add(output);
+            }
+            catch (Exception ex)
+            {
+                this.lstData.Items.Add(ex.ToString());
+            }
+        }
+    }
+}
